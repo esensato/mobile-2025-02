aac04a8932a9e7d0a12df0fbd835e03c2a279b0c
Edson Sensato <esensato@hotmail.com>
2025-09-10 21:43:09 -0300

    10/09/2025

--- aula/controlegastos/app/estilo.tsx
+++ aula/controlegastos/app/estilo.tsx
@@ -4,9 +4,70 @@ import { StyleSheet } from "react-native";
 export const estilo = StyleSheet.create({
     container: {
         flex: 1, // ocupa toda a dimensão vertical
-        marginTop: '10%',
-        borderWidth: 5,
-        borderColor: 'red',
         padding: 10
-    }
+    },
+    input: {
+        height: 50,
+        width: "100%",
+        marginVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        backgroundColor: "#f9f9f9",
+        shadowColor: "#c21313",
+        shadowOffset: {
+            width: 0,
+            height: 8,
+        },
+        shadowOpacity: 0.21,
+        shadowRadius: 8.19,
+        elevation: 11,
+        fontSize: 16,
+        color: "#333",
+    },
+    inputFocused: {
+        borderColor: "#4A90E2",
+        backgroundColor: "#fff",
+    },
+    button: {
+        backgroundColor: "#4A90E2",
+        paddingVertical: 14,
+        paddingHorizontal: 25,
+        borderRadius: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.15,
+        shadowRadius: 3.5,
+        elevation: 3,
+        marginVertical: 10,
+        width: "100%",
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "600",
+    },
+    buttonPressed: {
+        backgroundColor: "#357ABD"
+    },
+    topSection: {
+        alignItems: "center",
+    },
+    listSection: {
+        flex: 1,
+        marginTop: 10,
+        backgroundColor: "#ffff21",
+        borderRadius: 12,
+        padding: 15,
+    },
+    listPlaceholder: {
+        color: "#666",
+        fontSize: 14,
+        fontStyle: "italic",
+        textAlign: "center",
+        marginTop: 10,
+    },
 });
--- aula/controlegastos/app/index.tsx
+++ aula/controlegastos/app/index.tsx
@@ -1,23 +1,58 @@
 
-import { View, Text, Button } from "react-native";
+import { View, Text, Pressable, TextInput, StatusBar } from "react-native";
 import { useState } from "react";
 import { estilo } from "./estilo";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
 
-  // contador = somente leitura armazena o valor atual
-  // incrementar = função para alterar (escrita) o valor atual de contador
-  let [contador, incrementar] = useState(1);
+  type Gasto = { id: number, descricao: string, valor: number };
 
-  function contar() {
-    incrementar(++contador);
-    console.log(contador);
+  let [titulo, setTitulo] = useState('');
+  let [gasto, addGasto] = useState<Gasto[]>([]);
+
+  function onTituloChange(textoDigitado: string) {
+    console.log(textoDigitado);
+    setTitulo(textoDigitado);
   }
 
-  return <View style={estilo.container}>
-    <Text>Valor Contador = {contador}</Text>
-    <Button title="Contar Pontos" onPress={contar} />
-  </View>
+  function addGastoHandler() {
+    // ["Gasto 1", "Gasto 2"]
+    // ...["Gasto 1", "Gasto 2"] = "Gasto1", "Gasto 2"
+    // "Gasto1", "Gasto 2", titulo = ["Gasto1", "Gasto 2", titulo]
+
+    //[{id:1, descricao: "Gasto 1", valor: 100}, {id:2, descricao: "Gasto 2", valor: 200}]
+    let gastosAtuais = [...gasto];
+    const novoGasto: Gasto = { id: Date.now(), descricao: titulo, valor: 0 };
+    gastosAtuais.push(novoGasto);
+    addGasto(gastosAtuais);
+  }
+
+  return <SafeAreaView style={{ flex: 1 }}>
+    <StatusBar />
+    <View style={estilo.container}>
+      {/* Área superior: input + botão */}
+      <View style={estilo.topSection}>
+        <TextInput style={estilo.input} onChangeText={onTituloChange} value={titulo} placeholder="Descrição do Gasto" />
+        <Pressable style={({ pressed }) => [estilo.button, pressed && estilo.buttonPressed]}
+          onPress={addGastoHandler} >
+          <Text style={estilo.buttonText}>Adicionar</Text>
+        </Pressable>
+
+      </View>
+
+      {/* Área inferior: lista de gastos */}
+      <View style={estilo.listSection}>
+        {gasto.map((item) => {
+          // { id: number, descricao: string, valor: number };
+          return <Text key={item.id} style={estilo.listPlaceholder}>{item.descricao} {item.valor}</Text>
+        })}
+      </View>
+    </View>
+
+  </SafeAreaView>
+
+
 }
 
 
